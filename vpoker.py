53e557d3425afecaafb00e6c3a57ffd890b29e2d
kolyat <kks.pub@gmail.com>
2016-10-21 01:21:45 +0300

    Show main window with background

--- vpoker.py
+++ vpoker.py
@@ -11,13 +11,26 @@ from pygame.locals import *
 
 
 DISPLAY_MODE = (640, 480)
-BACKGROUND_COLOR = (11, 111, 0)
-
+FPS = 24
+BACKGROUND_COLOR = (0, 65, 15)  # (0, 25, 50) - dark blue
 
 # Initialize library and create main window
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode(DISPLAY_MODE)
 pygame.display.set_caption('Video Poker - Tens or Better')
+# Draw background
 background = pygame.Surface(screen.get_size())
-background = background.convert()
 background.fill(BACKGROUND_COLOR)
+background = background.convert()
+screen.blit(background, (0, 0))
+# Main event loop
+mainloop = True
+while mainloop:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            mainloop = False
+    pygame.display.flip()
+
+pygame.quit()
