1171547642fe66c46241fbf9d2a5cfcc6be9e436
Kolyat <kks.pub@gmail.com>
2017-01-29 01:19:19 +0300

    Update descriptions and text

--- test/test_vpoker.py
+++ test/test_vpoker.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2016-2017 Kirill 'Kolyat' Kiselnikov
 # This file is the part of vpoker, released under modified MIT license
 # See the file LICENSE.txt included in this distribution
 
