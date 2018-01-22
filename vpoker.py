f44caa6f16f17e01a473d975f95d9de0fd666636
Kolyat <kks.pub@gmail.com>
2018-01-23 02:03:54 +0300

    Refactoring

--- vpoker.py
+++ vpoker.py
@@ -1,93 +1,27 @@
-# Copyright (c) 2016-2017 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2016-2018 Kirill 'Kolyat' Kiselnikov
 # This file is the part of vpoker, released under modified MIT license
 # See the file LICENSE.txt included in this distribution
 
-"""
-Main module of vpoker
-"""
+"""Main module of vpoker"""
 
 import sys
 import os
 import random
 import time
+import logging
 import pygame
 from pygame.locals import *
-
-#
-# Graphical constants
-#
-FPS = 24
-ANIMATION_SPEED = 0.4
-# Table dimensions
-COMBINATION_CELL_WIDTH = 180
-WINNING_CELL_WIDTH = 80
-CELL_HEIGHT = 30
-BORDER_WIDTH = 2
-# Card dimensions
-ORIGIN_CARD_WIDTH = 720
-ORIGIN_CARD_HEIGHT = 1080
-K = 0.25
-# CARD_BACKGROUND_WIDTH = 94
-# CARD_BACKGROUND_HEIGHT = 140
-CARD_WIDTH = int(ORIGIN_CARD_WIDTH * K)
-CARD_HEIGHT = int(ORIGIN_CARD_HEIGHT * K)
-CARD_BORDER = 10
-CARD_BACKGROUND_WIDTH = CARD_WIDTH + CARD_BORDER
-CARD_BACKGROUND_HEIGHT = CARD_HEIGHT + CARD_BORDER
-# Surfaces dimensions
-INDENTATION = 20
-TABLE_SURFACE_X = TABLE_SURFACE_Y = 0
-TABLE_HEIGHT = CELL_HEIGHT * 9
-CARDS_SURFACE_X = TABLE_SURFACE_X
-CARDS_SURFACE_Y = TABLE_HEIGHT + INDENTATION*2
-DISTANCE_BETWEEN_CARDS = int(CARD_BACKGROUND_WIDTH/10)
-# Screen dimensions
-SCREEN_WIDTH = CARD_BACKGROUND_WIDTH*5 + DISTANCE_BETWEEN_CARDS*4 + \
-               INDENTATION*2
-TABLE_SURFACE_SIZE = (SCREEN_WIDTH, TABLE_HEIGHT + INDENTATION*2)
-CARDS_SURFACE_SIZE = (SCREEN_WIDTH, CARD_BACKGROUND_HEIGHT + INDENTATION*2)
-DISPLAY_MODE = (SCREEN_WIDTH, TABLE_SURFACE_SIZE[1] + CARDS_SURFACE_SIZE[1])
-# Table coordinates
-TABLE_X = SCREEN_WIDTH/2 - (COMBINATION_CELL_WIDTH + WINNING_CELL_WIDTH*5)/2
-TABLE_Y = TABLE_SURFACE_Y + INDENTATION
-# Colors
-BACKGROUND_COLOR = (0, 65, 15)  # (0, 25, 50) - dark blue
-TABLE_BORDER_COLOR = FONT_COLOR = CARD_BACKGROUND_COLOR = (175, 175, 0)
-TABLE_SELECTED_COLOR = (0, 100, 20)
-CARD_ACTIVE_COLOR = (208, 113, 30)
-CARD_FONT_COLOR = WIN_FONT_COLOR = (0, 0, 0)
-WIN_COLOR = (200, 80, 30)
-#
-# Font paths and options
-#
-DATA_DIR = 'data'
-FONT_NAME = r'fonts\LiberationSans-Regular.ttf'
-FONT_SIZE = 16
-ANTIALIASING = 0
+from settings import *
 
 
 class Card(object):
     """Represents playing card"""
 
     def __init__(self, centerx=0):
-        """
-        Class Card constructor
+        """Class Card constructor
 
         :param centerx: x-coordinate of card's center, default = 0
-        :type: int
-
-        :arg centery: y-coordinate of card's center
-        :type: int
-        :arg active: currently selected card
-        :type: bool
-        :arg back: will show cards back if active (card is not defined)
-        :type: bool
-        :arg suit: card's suit
-        :type: str
-        :arg rank: card's rank
-        :type: str
-        :arg: held: if kept in hand
-        :type: bool
+        :type centerx: int
         """
         if type(centerx) not in (int, float):
             self.centerx = 0
@@ -98,15 +32,18 @@ class Card(object):
         else:
             self.centerx = int(centerx)
         self.centery = int(CARD_BACKGROUND_HEIGHT/2)
+        # If card is now selected
         self.active = False
+        # Show card's back when card is not defined
         self.back = True
         self.suit = ''
         self.rank = ''
+        # If stays on hand
         self.held = False
 
     def draw(self):
-        """Draw card with background and text"""
-
+        """Draw card with background and text
+        """
         # Draw card's background
         background_rect = pygame.Rect(
             self.centerx - int(CARD_BACKGROUND_WIDTH/2),
@@ -118,15 +55,14 @@ class Card(object):
         else:
             pygame.draw.rect(cards_surface, CARD_BACKGROUND_COLOR,
                              background_rect, 0)
-
         # Try to load card's image
         if self.back:
             try:
                 raw_image = pygame.image.load(
                     os.path.join(DATA_DIR, ''.join('BACK') + '.png'))
             except pygame.error:
-                print('Cannot load image: ',
-                      os.path.join(DATA_DIR, ''.join('BACK') + '.png'))
+                logging.error('Cannot load image: %s',
+                              os.path.join(DATA_DIR, ''.join('BACK') + '.png'))
                 raw_image = None
         else:
             try:
@@ -134,14 +70,16 @@ class Card(object):
                     os.path.join(DATA_DIR, ''.join(
                         self.suit + self.rank) + '.png'))
             except pygame.error:
-                print('Cannot load image: ',
-                      os.path.join(DATA_DIR, (self.suit + self.rank + '.png')))
+                logging.error(
+                    'Cannot load image: %s',
+                    os.path.join(DATA_DIR, (self.suit + self.rank + '.png'))
+                )
                 raw_image = None
         if raw_image:
             card_image = pygame.transform.scale(raw_image,
                                                 (CARD_WIDTH, CARD_HEIGHT))
         else:
-            print('Using text instead')
+            logging.error('Using text instead of image')
             if self.back:
                 card_image = card_font.render('BACK', ANTIALIASING,
                                               CARD_FONT_COLOR)
@@ -154,7 +92,6 @@ class Card(object):
         card_rect.centery = self.centery
         cards_surface.blit(card_image, (card_rect.left, card_rect.top))
         screen.blit(cards_surface, (CARDS_SURFACE_X, CARDS_SURFACE_Y))
-
         # Draw status text
         if self.held:
             status = font.render('HELD', ANTIALIASING, FONT_COLOR)
@@ -168,17 +105,15 @@ class Card(object):
         pygame.display.flip()
 
     def set_card(self, current_card):
-        """
-        Set card from playing deck
+        """Set card from playing deck
 
         :param current_card: card from deck
-        :type: tuple
+        :type current_card: tuple
 
-        :raises TypeError: if 'current_card' is not tuple
-        :raises ValueError: if length of 'current_card' is not equal to 2
-        :raises KeyError: if 'current_card' contains not valid suit/rank
+        :raise TypeError: 'current_card' is not tuple
+        :raise ValueError: length of 'current_card' is not equal to 2
+        :raise KeyError: 'current_card' contains not valid suit/rank
         """
-
         if type(current_card) != tuple:
             raise TypeError
         if len(current_card) != 2:
@@ -187,100 +122,83 @@ class Card(object):
             raise KeyError
         if current_card[1] not in ranks:
             raise KeyError
-        self.suit = current_card[0]
-        self.rank = current_card[1]
+        self.suit, self.rank = current_card
         self.set_back(False)
 
     def get_suit(self):
-        """
-        Get card's suit
+        """Get card's suit
 
         :return: card's suit
-        :type: string
+        :type: str
         """
-
         return self.suit
 
     def get_rank(self):
-        """
-        Get card's rank
+        """Get card's rank
 
         :return: card's rank
-        :type: string
+        :type: str
         """
-
         return self.rank
 
     def set_active(self, active=False):
-        """
-        Set state of a card: active or not
+        """Set state of a card: active or inactive
 
         :param: active: card's state
-        :type: bool
+        :type active: bool
 
         :raises TypeError: if 'active' is not bool
         """
-
         if type(active) != bool:
             raise TypeError
         self.active = active
 
     def get_held(self):
-        """
-        Get 'held' status
+        """Get 'held' status
 
         :return: 'held' status
         :type: bool
         """
-
         return self.held
 
     def set_held(self, hold=False):
-        """
-        Set status 'held'
+        """Set status 'held'
 
         :param hold: 'held' status
-        :type: bool
+        :type hold: bool
 
         :raises TypeError: if 'hold' is not boolean
         """
-
         if type(hold) != bool:
             raise TypeError
         self.held = hold
 
     def set_back(self, back=True):
-        """
-        Sets card back parameter
+        """Set card's back parameter
 
         :param back: back parameter
-        :type: bool
+        :type back: bool
 
         :raises TypeError: if 'back' is not bool
         """
-
         if type(back) != bool:
             raise TypeError
         self.back = back
 
 
 def draw_table():
+    """Draw table with winning combinations
     """
-    Draw table with winning combinations
-    """
-
     # Draw surface for winning combinations table
     table_surface = pygame.Surface(TABLE_SURFACE_SIZE)
     table_surface.fill(BACKGROUND_COLOR)
     # table_surface = table_surface.convert()
-
     # Draw cell for winning combination's name
     combination_rect = pygame.Rect(TABLE_X, TABLE_Y, COMBINATION_CELL_WIDTH,
                                    CELL_HEIGHT)
     for name in combination_names:
         if name == win_combo:
-            pygame.draw.rect(table_surface, WIN_COLOR,
-                             combination_rect, 0)
+            pygame.draw.rect(table_surface, WIN_COLOR, combination_rect, 0)
         pygame.draw.rect(table_surface, TABLE_BORDER_COLOR, combination_rect,
                          BORDER_WIDTH)
         # Print combination's name
@@ -294,7 +212,6 @@ def draw_table():
                                    combination_rect.top, WINNING_CELL_WIDTH,
                                    CELL_HEIGHT)
         for i, item in enumerate(poker_winnings[name], start=1):
-
             if coins == i and name == win_combo:
                 pygame.draw.rect(table_surface, CARD_BACKGROUND_COLOR,
                                  winning_rect, 0)
@@ -326,32 +243,22 @@ def draw_table():
 
 
 def init_deck():
-    """
-    Initialize playing deck with 52 cards
+    """Initialize playing deck with 52 cards
 
     :return: deck with 52 cards
     :type: list of tuples
     """
-
-    raw_deck = []
-    for suit in suits:
-        for rank in ranks:
-            raw_card = list()
-            raw_card.append(suit)
-            raw_card.append(rank)
-            raw_deck.append(tuple(raw_card))
-    return raw_deck
+    return [(suit, rank) for suit in suits for rank in ranks]
 
 
 def game():
-    """Main function"""
-
+    """Main game function
+    """
     # Init variables
     clock = pygame.time.Clock()
     global coins
     global win_combo
     global cards_surface
-
     # Main game loop
     game_loop = True
     while game_loop:
@@ -367,12 +274,12 @@ def game():
                        (CARD_BACKGROUND_WIDTH + DISTANCE_BETWEEN_CARDS) * 5,
                        CARD_BACKGROUND_WIDTH + DISTANCE_BETWEEN_CARDS):
             cards.append(Card(x))
-
         # Draw surface for cards
         cards_surface.fill(BACKGROUND_COLOR)
         # cards_surface = cards_surface.convert()
-
+        #
         # Stage 1: insert coins to select amount of winning
+        #
         draw_table()
         for card in cards:
             card.draw()
@@ -395,8 +302,9 @@ def game():
                 if event.type == QUIT:
                     pygame.quit()
                     exit(0)
-
+        #
         # Stage 2: hand out cards, wait for player to hold some cards
+        #
         active_card = 0
         for card in cards:
             random_card = deck.pop(random.randint(0, len(deck)-1))
@@ -436,23 +344,26 @@ def game():
                     exit(0)
         cards[active_card].set_active(False)
         cards[active_card].draw()
-
+        #
         # Stage 3: remove cards that were not held
+        #
         for card in cards:
             if not card.get_held():
                 card.set_back(True)
                 card.draw()
         time.sleep(ANIMATION_SPEED)
-
+        #
         # Stage 4: hand out new cards
+        #
         for card in cards:
             if not card.get_held():
                 random_card = deck.pop(random.randint(0, len(deck)-1))
                 card.set_card(random_card)
                 card.draw()
                 time.sleep(ANIMATION_SPEED)
-
+        #
         # Stage 5: check for winning combinations and show if any
+        #
         card_suits = list()
         card_ranks = list()
         for card in cards:
@@ -478,13 +389,11 @@ def game():
 
 
 def menu():
-    """
-    Video poker type selection menu
+    """Video poker type selection menu
 
     :return: name of poker type
-    :type: string
+    :type: str
     """
-
     def clrscr():
         """Clear screen"""
         if sys.platform.startswith('linux') or \
@@ -500,7 +409,6 @@ def menu():
         '2': 'Jacks or Better'
     }
     item = ''
-
     while item not in menu_items:
         clrscr()
         print()
@@ -531,6 +439,14 @@ if __name__ == 'vpoker':
     ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
 
 if __name__ == '__main__':
+    # Init logger
+    logging.basicConfig(
+        filename='vpoker.log',
+        filemode='w',
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.ERROR
+    )
     # Select poker type
     m = menu()
     if m == 'Tens or Better':
@@ -551,7 +467,7 @@ if __name__ == '__main__':
         card_font = pygame.font.Font(os.path.join(DATA_DIR, FONT_NAME),
                                      FONT_SIZE * 2)
     except OSError as sys_error:
-        print('File not found: ', os.path.join(DATA_DIR, FONT_NAME))
+        logging.error('File not found: %s', os.path.join(DATA_DIR, FONT_NAME))
         font = pygame.font.SysFont('None', FONT_SIZE)
         card_font = pygame.font.SysFont('None', FONT_SIZE * 2)
     #
