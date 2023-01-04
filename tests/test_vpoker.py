958ef750f5d95571677121bfaf70f7701ed0539b
Kolyat <kks.pub@gmail.com>
2023-01-05 00:03:41 +0300

    New Year 2023

--- tests/test_vpoker.py
+++ tests/test_vpoker.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2022 Kirill 'Kolyat' Kiselnikov
+# Copyright (c) 2016-2023 Kirill 'Kolyat' Kiselnikov
 # This file is the part of vpoker, released under modified MIT license
 # See the file LICENSE.txt included in this distribution
 
